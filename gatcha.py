e73af1cb6c60e90126f6845b435c76c21a7aa7eb
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-06 00:33:28 +0900

    create gatcha.py

---
+++ gatcha.py
@@ -0,0 +1,19 @@
+#-*- encoding: cp932
+import card
+import random
+
+class Gatcha:
+    def __init__(self, ssr_prob, sr_prob):
+        self.ssr_prob = ssr_prob
+        self.sr_prob = sr_prob
+
+    def __getRandom(self):
+        return random.uniform(0, 100)
+
+    def twist1(self):
+        choice = self.__getRandom()
+        if choice > self.ssr_prob:
+            return card.SSRare()
+        elif choice > self.sr_prob:
+            return card.SRare()
+        return card.Rare()
