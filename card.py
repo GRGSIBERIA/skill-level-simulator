fa2314f4e8cc89fc6f838f8f1d6bd233b47a7fc1
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-05 23:53:09 +0900

    add card and rarity class

---
+++ card.py
@@ -0,0 +1,11 @@
+#-*- encoding: cp932
+
+class Rarity:
+    SSRare = 0
+    SRare = 1
+    Rare = 2
+
+class Card:
+    def __init__(self, rarity):
+        self.skill_level = 1
+        self.rarity = rarity
