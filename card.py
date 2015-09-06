26daa4677cd2a54c97c387a050506fb71c1d38cc
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-06 12:51:16 +0900

    add skill level

--- card.py
+++ card.py
@@ -1,18 +1,28 @@
 #-*- encoding: cp932
 
 class Card:
-    def __init__(self, rarity):
+    def __init__(self, rarity, point):
         self.skill_level = 1
         self.rarity = rarity
+        self.point = point
+
+    def skillup(self):
+        self.skill_level += 1
+        if self.rarity == "SSRare":
+            self.point = self.skill_level * 4 * 2
+        elif self.rarity == "SRare":
+            self.point = self.skill_level * 4
+        else:
+            self.point = self.skill_level
 
 class SSRare(Card):
     def __init__(self):
-        Card.__init__(self, "SSRare")
+        Card.__init__(self, "SSRare", 40)
 
 class SRare(Card):
     def __init__(self):
-        Card.__init__(self, "SRare")
+        Card.__init__(self, "SRare", 4)
 
 class Rare(Card):
     def __init__(self):
-        Card.__init__(self, "Rare")
+        Card.__init__(self, "Rare", 1)
