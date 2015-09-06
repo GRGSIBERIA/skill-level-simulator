4142e589592fa6b79d6bd6b79da01b07ff5dc6c6
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-06 13:22:20 +0900

    必要ポイント数を追加

--- card.py
+++ card.py
@@ -1,15 +1,23 @@
 #-*- encoding: cp932
 
 class Card:
+    def __setNeedPoint(self):
+        if self.rarity == "SSRare":
+            self.need_point = self.skill_level * 4 * 2
+        elif self.rarity == "SRare":
+            self.need_point = self.skill_level * 2
+        elif self.rarity == "Rare":
+            self.need_point = self.skill_level
+
     def __init__(self, rarity, base_point):
         self.skill_level = 1
         self.rarity = rarity
         self.base_point = base_point
-        self.point = base_point
+        self.need_point = self.__setNeedPoint()
 
     def skillup(self):
         self.skill_level += 1
-        self.point = self.skill_level * self.base_point
+        self.need_point = self.__setNeedPoint()
 
 class SSRare(Card):
     def __init__(self):
