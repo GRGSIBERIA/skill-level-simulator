124b8ce6d9762038343c0362892a9db1f0c2d389
Eiichi Takebuchi <nanashi4129@gmail.com>
2015-09-07 22:55:09 +0900

    ステータスの表示

---
+++ status.py
@@ -0,0 +1,11 @@
+#-*- encoding: utf-8
+
+class Status:
+    def __init__(self, deck, wallet):
+        self.deck = deck
+        self.wallet = wallet
+
+    def showStatus(self):
+        print "==============================="
+        
+
